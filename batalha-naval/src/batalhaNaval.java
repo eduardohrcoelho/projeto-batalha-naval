b1b0650b783469a57779db0c3d7beec0c973a38b
Eduardo Coelho <eduardohrcoelhomkt@gmail.com>
2025-07-14 11:39:43 -0300

    Otimização e criação das funções de inicializar e imprimir os tabuleiros

--- batalha-naval/src/batalhaNaval.java
+++ batalha-naval/src/batalhaNaval.java
@@ -1,58 +1,45 @@
 public class batalhaNaval {
+    // Função para incializar qualquer tabuleiro
+    public static void inicializarTabuleiro(char[][] tabuleiro){
+        // Preenche o tabuleiro com "~"
+        for(int i=0; i < tabuleiro.length; i++){
+            for(int j=0; j < tabuleiro[i].length; j++){
+                tabuleiro[i][j] = '~';
+            }
+        }
+    }
+    // Função para imprimir qualquer tabuleiro
+    public static void imprimirTabuleiro(char[][] tabuleiro){
+        for(int i=0; i < tabuleiro.length; i++){
+            for(int j=0; j < tabuleiro[i].length; j++){
+                System.out.print(tabuleiro[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
     public static void main(String[] args) throws Exception {
         // String tabuleiro[][] = new String[8][8]; Cria um tabuleiro 8x8
-        String tabuleiroDef01[][] = new String[8][8]; //Tabuleiro de defesa do jogador 01
-        String tabuleiroAtac01[][] = new String[8][8]; //Tabuleiro de ataque do jogador 01
-        String tabuleiroDef02[][] = new String[8][8]; //Tabuleiro de defesa do jogador 02
-        String tabuleiroAtac02[][] = new String[8][8]; //Tabuleiro de ataque do jogador 02
+        // Criar tabuleiros
+        char tabuleiroDef01[][] = new char[8][8]; //Tabuleiro de defesa do jogador 01
+        char tabuleiroAtac01[][] = new char[8][8]; //Tabuleiro de ataque do jogador 01
+        char tabuleiroDef02[][] = new char[8][8]; //Tabuleiro de defesa do jogador 02
+        char tabuleiroAtac02[][] = new char[8][8]; //Tabuleiro de ataque do jogador 02
 
+        // Inicialização dos tabuleiros usando a função inicializarTabuleiro
+        inicializarTabuleiro(tabuleiroAtac01);
+        inicializarTabuleiro(tabuleiroDef01);
+        inicializarTabuleiro(tabuleiroAtac02);
+        inicializarTabuleiro(tabuleiroDef02);
 
-        // Preenche o tabuleiro de Defesa com "~"
-        for(int i=0; i < tabuleiroDef01.length; i++){
-            for(int j=0; j < tabuleiroDef01[i].length; j++){
-                tabuleiroDef01[i][j] = "~";
-                tabuleiroAtac01[i][j] = "~";
-                tabuleiroDef02[i][j] = "~";
-                tabuleiroAtac02[i][j] = "~";
-            }
-        }
-
-        // Imprime o tabuleiro de Defesa 01
-        for(int i=0; i < tabuleiroDef01.length; i++){
-            for(int j=0; j < tabuleiroDef01[i].length; j++){
-                System.out.print(tabuleiroDef01[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        System.out.println("=================================");
-
-        // Imprime o tabuleiro de Defesa 02
-        for(int i=0; i < tabuleiroDef02.length; i++){
-            for(int j=0; j < tabuleiroDef02[i].length; j++){
-                System.out.print(tabuleiroDef02[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        System.out.println("=================================");
-
-        // Imprime o tabuleiro de Ataque 01
-        for(int i=0; i < tabuleiroAtac01.length; i++){
-            for(int j=0; j < tabuleiroAtac01[i].length; j++){
-                System.out.print(tabuleiroAtac01[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        System.out.println("=================================");
-
-        // Imprime o tabuleiro de Ataque 02
-        for(int i=0; i < tabuleiroAtac02.length; i++){
-            for(int j=0; j < tabuleiroAtac02[i].length; j++){
-                System.out.print(tabuleiroAtac02[i][j] + " ");
-            }
-            System.out.println();
-        }
+        // Imprime os tabuleiros
+        System.out.println("--- Tabuleiro de Defesa 01 ---");
+        imprimirTabuleiro(tabuleiroDef01);
+        System.out.println("\n--- Tabuleiro de Ataque 01 ---");
+        imprimirTabuleiro(tabuleiroAtac01);
+        System.out.println("\n--- Tabuleiro de Defesa 02 ---");
+        imprimirTabuleiro(tabuleiroDef02);
+        System.out.println("\n--- Tabuleiro de Ataque 02 ---");
+        imprimirTabuleiro(tabuleiroAtac02);
     }
 }
